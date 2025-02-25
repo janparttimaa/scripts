a2460813d6e80aff0b7e453d78e112a6a7f76a1d
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2025-02-25 09:20:08 +0200

    Add files via upload

---
+++ Windows/Edition upgrade to Windows 10_11 Enterprise via KMS/upgrade.ps1
@@ -0,0 +1,30 @@
+﻿# Define Generic Volume License Key for Windows 10/11 Enterprise
+# Documentation: https://learn.microsoft.com/en-us/windows-server/get-started/kms-client-activation-keys?tabs=server2025%2Cwindows1110ltsc%2Cversion1803%2Cwindows81
+$kmsKey = "NPPR9-FWDCX-D2C8J-H872K-2YT43"
+
+# Function to check Windows edition
+function Get-WindowsEdition {
+    $osEdition = (Get-WmiObject -Class Win32_OperatingSystem).OperatingSystemSKU
+    return $osEdition
+}
+
+# Check if the device is on the local network
+$localNetwork = Test-Connection -ComputerName "fihelsms02.fi.kworld.kpmg.com" -Count 1 -Quiet
+
+if ($localNetwork) {
+    # Get the current Windows edition
+    $windowsEdition = Get-WindowsEdition
+
+    # Check if the Windows edition is not Enterprise (4 is the SKU for Enterprise)
+    if ($windowsEdition -ne 4) {
+        # Install the KMS key silently
+        cscript C:\Windows\System32\slmgr.vbs -ipk $kmsKey
+        Write-Output "KMS key installed successfully."
+        [System.Environment]::Exit(0)
+    } else {
+       Write-Output "Windows is running Enterprise edition. KMS key installation skipped."
+       [System.Environment]::Exit(0)
+    }
+} else {
+    Write-Output "Device is not on the local network. KMS key installation aborted."
+}
