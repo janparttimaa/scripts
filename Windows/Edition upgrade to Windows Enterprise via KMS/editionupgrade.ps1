d2cb1beac371a94efd970ec0acb4716c5f309d7f
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2025-03-11 16:22:51 +0200

    Update editionupgrade.ps1

--- Windows/Edition upgrade to Windows Enterprise via KMS/editionupgrade.ps1
+++ Windows/Edition upgrade to Windows Enterprise via KMS/editionupgrade.ps1
@@ -15,17 +15,17 @@ if ($localNetwork) {
     # Get the current Windows edition
     $windowsEdition = Get-WindowsEdition
 
-    # Check if the Windows edition is not Enterprise (4 is the SKU for Enterprise)
+    # Check if the Windows edition is not Enterprise (4 is the SKU for Enterprise). If not, it will be upgraded to Enterprise using Generic Volume License Key
     if ($windowsEdition -ne 4) {
         # Install the KMS key silently
         cscript C:\Windows\System32\slmgr.vbs -ipk $kmsKey
-        Write-Output "KMS key installed successfully."
+        Write-Host "KMS key installed successfully. Closing script..."
         [System.Environment]::Exit(0)
     } else {
-       Write-Output "Windows is running Enterprise edition. KMS key installation skipped."
+       Write-Host "Windows is running Enterprise edition. KMS key installation skipped. Closing script..."
        [System.Environment]::Exit(0)
     }
 } else {
-    Write-Output "Device is not on the local network. KMS key installation aborted."
+    Write-Host "Device is not on the local network. KMS key installation aborted. Closing script..."
     [System.Environment]::Exit(1)
 }
