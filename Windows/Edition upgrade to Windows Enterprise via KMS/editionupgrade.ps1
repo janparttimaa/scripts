d2ae37747fc3624401fafe1ba134e72132837301
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2025-03-12 09:59:14 +0200

    Add files via upload

--- Windows/Edition upgrade to Windows Enterprise via KMS/editionupgrade.ps1
+++ Windows/Edition upgrade to Windows Enterprise via KMS/editionupgrade.ps1
@@ -1,6 +1,41 @@
-﻿# Define Generic Volume License Key for Windows 10/11 Enterprise
+﻿<#﻿
+.SYNOPSIS
+    Edition Upgrade to Windows 10/11 Enterprise via KMS
+
+.DESCRIPTION
+    If you came across situation where your KMS-activated Windows 10/11 Enterprise have been somehow downgraded to e.g. Windows 10/11 Pro, this script is for you! 
+    This script upgrade your company-managed device back to KMS-activated Windows 10/11 Enterprise silently without any user interruption.
+    Scope of this script: Corporate environments only.
+    Platform: Windows 10 and later.
+
+.VERSION
+    20250311
+
+.AUTHOR
+    Jan Parttimaa (https://github.com/janparttimaa)
+
+.COPYRIGHT
+    © 2025 Jan Parttimaa. All rights reserved.
+
+.LICENSE
+    This script is licensed under the MIT License.
+    You may obtain a copy of the License at https://opensource.org/licenses/MIT
+
+.RELEASE NOTES
+    20250312 - Initial release
+
+.EXAMPLE
+    powershell.exe -ExecutionPolicy Bypass -File .\editionupgrade.ps1
+    This example is how to run this script running Windows PowerShell. Run this command with your admin rights.
+    This is also the command that needs to be use when deploying it via Microsoft Configuration Manager or via Intune.
+#>
+
+# Define Generic Volume License Key for Windows 10/11 Enterprise
 # Documentation: https://learn.microsoft.com/en-us/windows-server/get-started/kms-client-activation-keys?tabs=server2025%2Cwindows1110ltsc%2Cversion1803%2Cwindows81
 $kmsKey = "NPPR9-FWDCX-D2C8J-H872K-2YT43"
+
+# Define name of the server (e.g. SCCM-server) that can be pinged only from local network
+$internalserver = "internalserver"
 
 # Function to check Windows edition
 function Get-WindowsEdition {
@@ -9,7 +44,7 @@ function Get-WindowsEdition {
 }
 
 # Check if the device is on the local network
-$localNetwork = Test-Connection -ComputerName "internalserver.example.com" -Count 1 -Quiet
+$localNetwork = Test-Connection -ComputerName "$internalserver.example.com" -Count 1 -Quiet
 
 if ($localNetwork) {
     # Get the current Windows edition
@@ -20,12 +55,15 @@ if ($localNetwork) {
         # Install the KMS key silently
         cscript C:\Windows\System32\slmgr.vbs -ipk $kmsKey
         Write-Host "KMS key installed successfully. Closing script..."
+        # Exit code for SCCM and Intune
         [System.Environment]::Exit(0)
     } else {
-       Write-Host "Windows is running Enterprise edition. KMS key installation skipped. Closing script..."
-       [System.Environment]::Exit(0)
+        Write-Host "Windows is running Enterprise edition. KMS key installation skipped. Closing script..."
+        # Exit code for SCCM and Intune
+        [System.Environment]::Exit(0)
     }
 } else {
     Write-Host "Device is not on the local network. KMS key installation aborted. Closing script..."
+    # Exit code for SCCM and Intune
     [System.Environment]::Exit(1)
 }
