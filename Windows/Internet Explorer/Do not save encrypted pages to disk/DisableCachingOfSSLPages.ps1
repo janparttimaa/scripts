f9cacf0b29d5eff959d67fd07dc8ddedcb7d2876
Jan Parttimaa <jan.parttimaa@hotmail.fi>
2026-01-24 12:39:23 +0200

    New changes.

--- Windows/Internet Explorer/Do not save encrypted pages to disk/DisableCachingOfSSLPages.ps1
+++ Windows/Internet Explorer/Do not save encrypted pages to disk/DisableCachingOfSSLPages.ps1
@@ -40,7 +40,7 @@
 .EXAMPLE
     Run the following command with administrative privileges:
 
-    powershell.exe -ExecutionPolicy Bypass -File .\DisableCachingOfSSLPages.ps1
+    powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "DisableCachingOfSSLPages.ps1"
 
     This is the recommended execution method when deploying the script via
     Microsoft Intune, or Microsoft Configuration Manager.
