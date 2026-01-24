be91300edf1e8aabd09b338cf3c76b8cea4e570a
Jan Parttimaa <jan.parttimaa@hotmail.fi>
2026-01-24 16:54:13 +0200

    New changes.

--- Windows/Point and Print/Computer-RpcAuthnLevelPrivacyEnabled.ps1
+++ Windows/Point and Print/Computer-RpcAuthnLevelPrivacyEnabled.ps1
@@ -17,25 +17,25 @@
         https://support.microsoft.com/en-us/topic/kb5005652-manage-new-point-and-print-default-driver-installation-behavior-cve-2021-34481-873642bf-2634-49c5-a23b-6d8e9a302872
 
 .VERSION
-    20251224
+    20260124
 
 .AUTHOR
     Jan Parttimaa
 
 .COPYRIGHT
-    © 2025 Jan Parttimaa. All rights reserved.
+    © 2026 Jan Parttimaa. All rights reserved.
 
 .LICENSE
     This script is licensed under the MIT License.
     You may obtain a copy of the License at https://opensource.org/licenses/MIT
 
 .RELEASE NOTES
-    20251224 - Initial release
+    20260124 - Initial release
 
 .EXAMPLE
     Run the following command with administrative privileges:
 
-    powershell.exe -ExecutionPolicy Bypass -File .\RpcAuthnLevelPrivacyEnabled.ps1
+    %windir%\sysnative\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "Computer-RpcAuthnLevelPrivacyEnabled.ps1"
 
     This is the recommended execution method when deploying the script via
     Microsoft Intune, or Microsoft Configuration Manager.
